9e2744beb233c82c92339d1b45c486a084a5d22a
dmsumon2020 <dmsumon2020@gmail.com>
2025-05-11 12:56:00 +0600

    fixed firebase issue

--- src/FIrebase/Firebase.js
+++ src/FIrebase/Firebase.js
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need//
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration//
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
 };
 
-// Initialize Firebase
+// Initialize Firebase//
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 export default auth;
